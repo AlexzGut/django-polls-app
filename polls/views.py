dcfe0d653c0e87a9acf19ab736e9b6eec2201184
Alexander Gutierrez <alexzgut.dev@gmail.com>
2024-08-24 18:28:46 -0400

    Created views for the polls app, added templates, and updated URL configurations

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, Universe - Polls Index Page')
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'lastest_question_list': lastest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    pass
+
+
+def vote(request, question_id):
+    pass
