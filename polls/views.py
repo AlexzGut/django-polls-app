f4a50f996645db67fe45623cceefcb0f484275d2
Alexander Gutierrez <alexzgut.dev@gmail.com>
2024-08-25 16:09:08 -0400

    Enhanced the polls app with generic views, improved the detail and results views, and added voting functionality.

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,51 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'lastest_question_list': lastest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    # By default Django will look for a template called -> <app name>/<model name>_list.html
+    template_name = 'polls/index.html' 
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+            Return the last five published questions
+        """
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    # By default Django will look for a template called -> <app name>/<model name>_detail.html
+    template_name = 'polls/detail.html'
+    model = Question
+    
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
-
-
-def results(request, question_id):
-    pass
+class ResultsView(generic.DetailView):
+    # By default Django will look for a template called -> <app name>/<model name>_detail.html
+    template_name = 'polls/results.html'
+    model = Question
 
 
 def vote(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_id)
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': 'Choice was not selected',
+            }
+        return render(request, 'polls:detail', context)
+    else:
+        # Django uses the F() object to generate an SQL expression that
+        # describes the required operation at the database level.
+        # In this case we are incrementing the votes value of the selected choice by 1.
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
